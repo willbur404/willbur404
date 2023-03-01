0a163048be2b5718cf3a4cafcc298f642edf1627
wuhongpeng <wuhongpeng@zuoyebang.com>
2023-03-01 18:45:15 +0800

    create database model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Cagegory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    excerpt = models.CharField(blank=True, max_length=200)
+
+    category = models.ForeignKey(Cagegory, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
