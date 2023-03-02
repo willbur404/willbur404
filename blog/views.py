2aea26124352aa177a994975fa9c69bd8e3b54f4
wuhongpeng <wuhongpeng@zuoyebang.com>
2023-03-02 17:37:18 +0800

    add pylint ignore member

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 
 def index(request):
+    post_list = Post.objects.all().order_by("-created_time")
     return render(
         request,
         "blog/index.html",
-        context={"title": "My blog", "welcome": "Welcome to my blog"},
+        context={"post_list": post_list},
     )
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-oxr@t5#*jj+-23utjsm$xmo+o8x@hvp&$i&v1szisvc7ia8%kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = list[str]
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
